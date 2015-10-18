4cafc8b62085351a2e79fb1544a534999e8b712b
alldayiwonder <carrea.steve@gmail.com>
2015-10-18 16:58:03 -0400

    Commit airEmissions reader

---
+++ read_airEmissions.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+#county_cancer = pd.read_csv('/Users/Steve/Github/ny_cancerAnalysis/data/NYSDOH_CancerMapping_Data_2005_2009/NYSDOH_CancerMapping_Data_2005_2009.csv')
+
+airEmissions = pd.read_csv()
