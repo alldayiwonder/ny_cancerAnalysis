702acb63b3338c73d91e3f1dc2b9b7fd6c8e6481
alldayiwonder <carrea.steve@gmail.com>
2015-10-21 20:19:56 -0400

    Add data

--- read_airEmissions.py
+++ read_airEmissions.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 #county_cancer = pd.read_csv('/Users/Steve/Github/ny_cancerAnalysis/data/NYSDOH_CancerMapping_Data_2005_2009/NYSDOH_CancerMapping_Data_2005_2009.csv')
 
-airEmissions = pd.read_csv()
+airEmissions = pd.read_csv('/Users/Steve/Github/ny_cancerAnalysis/data/EPA_TRI/enigma-us.gov.epa.toxic-release-inventory.ny.2013.csv')
+print airEmissions.describe()
