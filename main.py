4f466eac63c81bafebf725e28311416ce52201b2
stevecarrea <carrea.steve@gmail.com>
2015-10-25 15:12:00 -0400

    Updates main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 # Import cancer data
 cancerRates = read_cancerRates()
-cancerRates['County Name'] = map(lambda x: x.upper(), cancerRates['County Name'])  # Capitalize county names to match with other data for merge
+# Capitalize county names to match with other data for merge
+cancerRates['County Name'] = map(lambda x: x.upper(), cancerRates['County Name'])  
 
 # Import air data
 airEmissions = read_airEmissions()
@@ -12,6 +13,7 @@ airEmissions = read_airEmissions()
 # Join air emission data with cancer rates data
 data_merged = pd.merge(cancerRates, airEmissions, left_on = 'County Name', right_on = 'county')
 data_merged = data_merged.drop('county', 1)
-print data_merged
-
+print data_merged 
+print 
+print '============================ Correlation Table ============================'
 print data_merged.corr()
