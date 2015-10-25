4fc5d4a75652fb3e0fc186780e63381c7cc08081
stevecarrea <carrea.steve@gmail.com>
2015-10-25 14:51:36 -0400

    Updates data links and adds .gitignore
    Ignoring .pyc files

--- read_cancerRates.py
+++ read_cancerRates.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def read_cancerRates():
-	cancerRates = pd.read_csv('Community_Health__All_Cancer_Incidence_Age-adjusted_Rate_per_100_000_by_County_Maps__Latest_Data.csv')
+	cancerRates = pd.read_csv('data/Community_Health__All_Cancer_Incidence_Age-adjusted_Rate_per_100_000_by_County_Maps__Latest_Data.csv')
 	cancerRates_trim = cancerRates[['County Name', 'Percent/Rate']]
 	return cancerRates_trim
 
