584e635d6732e8a022271aefe13b6ebe25228579
andrewfair <andrew.fair@gmail.com>
2015-11-01 15:31:33 -0500

    make read ACS code flexible for county or tract

--- readACS.py
+++ readACS.py
@@ -1,16 +1,21 @@
 import pandas as pd
 
-def readCountyAcs():
-	acsCountyFile = 'data/ACS/R11053959_SL050.csv'
-	county = pd.read_csv(acsCountyFile)
+def readCountyAcs(geoLevel):
+	if geoLevel == 'county':
+		fn = 'data/ACS/R11053959_SL050.csv'
+	elif geoLevel == 'tract':
+		fn = 'data/ACS/R11053959_SL140.csv'
 
+	data = pd.read_csv(fn)
 	#taking fips, tot pop, male pop, age 65-74, 75-84, and 85+
-	countySub = county[['Geo_FIPS', 'SE_T001_001', 'SE_T004_002', 'SE_T007_011', 'SE_T007_012', 'SE_T007_013']]
-	countySub['pctMale'] = countySub['SE_T004_002'] / countySub['SE_T001_001']
-	countySub['pctElderly'] = (countySub['SE_T007_011'] + countySub['SE_T007_012']+ countySub['SE_T007_013'])/ countySub['SE_T001_001']
-	countySub = countySub.drop(['SE_T007_011', 'SE_T007_012', 'SE_T007_013', 'SE_T004_002'], 1)
-	newCol = countySub.columns.values
+	data = data[['Geo_FIPS', 'SE_T001_001', 'SE_T004_002', 'SE_T007_011', 'SE_T007_012', 'SE_T007_013']]
+	data['pctMale'] = data['SE_T004_002'] / data['SE_T001_001']
+	data['pctElderly'] = (data['SE_T007_011'] + data['SE_T007_012']+ data['SE_T007_013'])/ data['SE_T001_001']
+	data = data.drop(['SE_T007_011', 'SE_T007_012', 'SE_T007_013', 'SE_T004_002'], 1)
+	newCol = data.columns.values
 	newCol[1] = 'totPop'
-	countySub.columns = newCol
-	countySub['countyFIPS'] = countySub['Geo_FIPS'].str[2:5]
-	return acsCounty
+	data.columns = newCol
+	data['countyFIPS'] = data['Geo_FIPS'].str[2:5]
+	if geoLevel == 'tract':
+		data['tractFIPS'] = data['Geo_FIPS'].str[5:]
+	return data
