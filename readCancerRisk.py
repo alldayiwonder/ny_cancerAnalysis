19cf0a87054aaa355c6d3ddfa07dd99d4b5efa59
andrewfair <andrew.fair@gmail.com>
2015-11-28 18:02:23 -0500

    make cancer risk use relative paths

--- readCancerRisk.py
+++ readCancerRisk.py
@@ -6,12 +6,12 @@ def read_cancerRisk_CensusTract():
 	"""
 	Creates a dataframe from cancer risk data as obtained from EPA's National Air Toxics Assessment (NATA).
 	"""
-	cancerRisk = pd.read_csv('/Users/Steve/Github/ny_cancerAnalysis/data/EPA_NATA/US_NATA05_NY.csv')
+	cancerRisk = pd.read_csv('data/EPA_NATA/US_NATA05_NY.csv')
 	cancerRisk["TRACT"] = cancerRisk.TRACT.map("{:06}".format)
 
 	cancerRisk["GEOID"] = cancerRisk["FIPS"].astype(str) + cancerRisk["TRACT"]
 
-	cancerRisk.to_csv('/Users/Steve/Github/ny_cancerAnalysis/data/EPA_NATA/US_NATA05_NYgeoid.csv')
+	cancerRisk.to_csv('data/EPA_NATA/US_NATA05_NYgeoid.csv')
 	return cancerRisk 
 
 read_cancerRisk_CensusTract()
